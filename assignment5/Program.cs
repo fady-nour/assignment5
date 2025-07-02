b8854f27b6b0e60b73fcbeefbe809e375fe308a0
Fadynour <fadynour194@gmail.com>
2025-07-02 16:39:58 +0300

    Q2

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -28,11 +28,33 @@
             Console.WriteLine("b = " + b);
 
         }
+        public static int SumArr(int[] arr)
+        {
+            int sum = 0;
+             arr = new int[] { 8, 6, 2, 1, 4, 7 }; arr[2] = 500;
+            foreach(int i in arr)
+            {
+                sum += i;
+            
+            }
+            return sum;
+        }
+        public static int SumArr2(ref int[] arr)
+        {
+            int sum = 0;
+            arr = new int[] { 8, 6, 2, 1, 4, 7 }; arr[2] = 500;
+            foreach (int i in arr)
+            {
+                sum += i;
+
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
             #region Q1 value type refrence value  type value
             //int x = 5, y = 10;
-            
+
             //Swap(ref x, ref y);
             //Console.WriteLine("number after passing");
             //Console.WriteLine("x = " + x);
@@ -43,6 +65,16 @@
             //Console.WriteLine("number after passing");
             //Console.WriteLine("c = " + c);
             //Console.WriteLine("u = " + u);
+            #endregion
+            #region Q2 refrence type by value and by refrence
+            //int[] num = { 5, 6, 8, 7, 4, 1 };
+            //Console.WriteLine("before passing = " + num[2]);
+            //SumArr(num);
+            //Console.WriteLine("after passing = " + num[2]);
+
+            //Console.WriteLine("before passing = " + num[2]);
+            //SumArr2(ref num);
+            //Console.WriteLine("after passing = " + num[2]);
             #endregion
 
         }
